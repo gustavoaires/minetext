9d225956b6af29d1a8b8bb5001b74bdf6b460e0c
Gustavo Aires <gustavoairesmatos@gmail.com>
2017-03-30 22:07:21 -0300

    adding connection and tweet retrieval

--- mytweetsentiment/datacollect.py
+++ mytweetsentiment/datacollect.py
@@ -1,1 +1,44 @@
-# all the collect code goes here
+import twitter
+from twitterscraper import query_tweets
+import json
+import datetime
+
+class TwitterAccess(object):
+
+    def connect(self, consumer_key, consumer_secret,
+                oauth_token, oauth_token_secret):
+        """
+        :param consumer_key: 
+        :param consumer_secret: 
+        :param oauth_token: 
+        :param oauth_token_secret: 
+        :return: object to access the twitter api
+        all the request can be done using this object
+        """
+
+        auth = twitter.oauth.OAuth(oauth_token, oauth_token_secret,
+                                   consumer_key, consumer_secret)
+        return twitter.Twitter(auth=auth)
+
+    def getTweetsByDate(self, query, initial_date, final_date):
+        """
+        :param query: should contain all the words and can includ logic operators
+        ex: trump OR hillary
+        :param initial_date and final_date must be in the following format
+        ex: YYYY-mm-dd
+        :return: created_at value is based on the following format
+        ex: YYYY-mm-dd HH:mm:ss
+        """
+        final_query = query + " since:" + initial_date + " until:" + final_date
+
+        tweets = []
+
+        for tweet in query_tweets(final_query, 10)[:10]:
+            dict = {}
+            dict['id'] = tweet.id.encode('utf-8')
+            dict['text'] = tweet.text
+            dict['screen_name'] = tweet.user.encode('utf-8')
+            dict['created_at'] = str(tweet.timestamp)
+            tweets.append(dict)
+
+        return tweets
---
+++ mytweetsentiment/main.py
@@ -0,0 +1,31 @@
+import datacollect
+from twitterscraper import query_tweets
+from twitterscraper import main
+import json
+
+# test file
+
+def main():
+    CONSUMER_KEY = '4jtRRPl3WvYyzPvRnNKRaaLGr'
+    CONSUMER_SECRET = 'dFr8l0HDQ2RA3sQhiTjkMvJs9ML38BLkEoofDNr50tBEOCmZNA'
+    OAUTH_TOKEN = '98992106-OsffZtvTWPxeHCkA5G9golyjU4inlvb7XB6g0oDva'
+    OAUTH_TOKEN_SECRET = 'eD0RE8AyfTDmyYnOKfWLo3XvjILKktRJOIoQnlmHCmbip'
+
+    twitter_api = datacollect.TwitterAccess().connect(CONSUMER_KEY,CONSUMER_SECRET,
+                                           OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+
+    print twitter_api
+
+    query = "previdencia OR reforma da presidencia OR reforma da previdencia"
+    initial_date = "2017-03-15"
+    final_date = "2017-03-22"
+    tweets = datacollect.TwitterAccess().getTweetsByDate(query, initial_date, final_date)
+
+    for tweet in tweets:
+        print tweet
+
+    # final_query = query + " since:" + initial_date + " until:" + final_date
+    #
+    # for tweet in query_tweets(final_query, 10)[:10]:
+    #     print tweet.text.encode('utf-8')
+main()
