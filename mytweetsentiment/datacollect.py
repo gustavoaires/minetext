c49e0d92d7d0725d8bc1f7594bfd062a804368bf
Gustavo Aires <gustavoairesmatos@gmail.com>
2017-05-11 13:30:01 -0300

    data collet modification

--- mytweetsentiment/datacollect.py
+++ mytweetsentiment/datacollect.py
@@ -1,34 +1,34 @@
 import twitter
 from twitterscraper import query_tweets
-import json
-import datetime
 
 class TwitterAccess(object):
 
+    """
+    :param consumer_key: 
+    :param consumer_secret: 
+    :param oauth_token: 
+    :param oauth_token_secret: 
+    :return: object to access the twitter api
+    all the request can be done using this object
+    """
     def connect(self, consumer_key, consumer_secret,
                 oauth_token, oauth_token_secret):
-        """
-        :param consumer_key: 
-        :param consumer_secret: 
-        :param oauth_token: 
-        :param oauth_token_secret: 
-        :return: object to access the twitter api
-        all the request can be done using this object
-        """
-
         auth = twitter.oauth.OAuth(oauth_token, oauth_token_secret,
                                    consumer_key, consumer_secret)
         return twitter.Twitter(auth=auth)
 
+    """
+    all the results obtained with this method 
+    has just id, user, text and timestamp metadata
+
+    :param query: should contain all the words and can includ logic operators
+    ex: trump OR hillary
+    :param initial_date and final_date must be in the following format
+    ex: YYYY-mm-dd
+    :return: created_at value is based on the following format
+    ex: YYYY-mm-dd HH:mm:ss
+    """
     def getTweetsByDate(self, query, initial_date, final_date):
-        """
-        :param query: should contain all the words and can includ logic operators
-        ex: trump OR hillary
-        :param initial_date and final_date must be in the following format
-        ex: YYYY-mm-dd
-        :return: created_at value is based on the following format
-        ex: YYYY-mm-dd HH:mm:ss
-        """
         final_query = query + " since:" + initial_date + " until:" + final_date
 
         tweets = []
@@ -42,3 +42,6 @@ class TwitterAccess(object):
             tweets.append(dict)
 
         return tweets
+
+    def getTweetsWithinSevenDays(self):
+        pass
