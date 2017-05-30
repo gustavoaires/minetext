6b849eee405212338fd45c4c24f569e8ca5ae82f
Gustavo Aires <gustavoairesmatos@gmail.com>
2017-05-29 22:50:37 -0300

    adding core value and general enhacement

--- senttext/__init__.py
+++ senttext/__init__.py
@@ -10,8 +10,8 @@ def main():
     # ACCESS_TOKEN = '98992106-OsffZtvTWPxeHCkA5G9golyjU4inlvb7XB6g0oDva'
     # ACCESS_TOKEN_SECRET = 'eD0RE8AyfTDmyYnOKfWLo3XvjILKktRJOIoQnlmHCmbip'
 
-    input_file = 'dataset_until_22_05_pln.json'
-    output_file = 'dataset_until_22_05_pln.csv'
+    input_file = 'tweets_22_05.json'
+    output_file = 'tweets_22_05_pln.tsv'
 
     # query = "previdencia social OR reforma da previdencia OR reforma da presidencia ' \
     #         'OR previdencia since:2017-05-23 until:2017-05-24"
@@ -25,29 +25,29 @@ def main():
     #     json.dump(tweet, output_file)
     #     output_file.write("\n")
 
-    # cleaner = TextCleaner()
-    # named = NamedEntity()
+    cleaner = TextCleaner()
+    named = NamedEntity()
     json_management = JSONFileManagement()
     csv_management = CSVTSVFileManagement()
 
     print 'reading1'
     tweets = json_management.read_file(input_file)
 
-    # new_tweets = []
+    new_tweets = []
 
     print 'writing1'
-    # for tweet in tweets:
-    #     text = tweet['text']
-    #     text = text.lower()
-    #     text = named.removeTwitterUsername(text)
-    #     text = cleaner.removeStopwords(text)
-    #     text = cleaner.removeSymbols(text)
-    #     text = cleaner.removeLinks(text)
-    #     text = cleaner.removeSufPort(text)
-    #     text = cleaner.removeAccent(text)
-    #     tweet['text'] = text
-    #     new_tweets.append(tweet)
+    for tweet in tweets:
+        text = tweet['text']
+        text = text.lower()
+        text = named.removeTwitterUsername(text)
+        text = cleaner.removeStopwords(text)
+        text = cleaner.removeSymbols(text)
+        text = cleaner.removeLinks(text)
+        text = cleaner.removeSufPort(text)
+        text = cleaner.removeAccent(text)
+        tweet['text'] = text
+        new_tweets.append(tweet)
 
-    csv_management.write_file(output_file, tweets)
+    csv_management.write_file(output_file, new_tweets, delimiter='\t')
 
 main()
--- senttext/clustering/dbscan.py
+++ senttext/clustering/dbscan.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 from datetime import datetime
-from datetime import timedelta
-import math
-from math import sqrt
+from math import sqrt, exp
+from random import shuffle
 
 progress = 0
+
 
 def euclideanDistance(point, point2):
     x = float(point['latitude'])
@@ -12,45 +12,44 @@ def euclideanDistance(point, point2):
     y = float(point['longitude'])
     y1 = float(point2['longitude'])
 
-    return sqrt((x - x1)**2 + ( y - y1)**2)
+    return sqrt((x - x1) ** 2 + (y - y1) ** 2)
+
 
 def fadingDistance(text1, text2):
-
     FMT = '%H:%M:%S'
     if datetime.strptime(text1['time'], FMT) > datetime.strptime(text2['time'], FMT):
         tdelta = datetime.strptime(text1['time'], FMT) - datetime.strptime(text2['time'], FMT)
     else:
         tdelta = datetime.strptime(text2['time'], FMT) - datetime.strptime(text1['time'], FMT)
 
-    timeDifference = tdelta.seconds/60.0/60
+    timeDifference = tdelta.seconds / 60.0 / 60
 
     words1 = set(text1['text'].split())
     words2 = set(text2['text'].split())
 
     duplicates = words1.intersection(words2)
     uniques = words1.union(words2.difference(words1))
-    
+
     try:
-        simi = float(len(duplicates))/(len(uniques) * math.exp(timeDifference))
+        simi = float(len(duplicates)) / (len(uniques) * exp(timeDifference))
         return simi
     except:
-        print "Error to get similarity"
         return 0.0
 
-def jaccardDistance(text1, text2):
 
+def jaccardDistance(text1, text2):
     words1 = set(text1['text'].split())
     words2 = set(text2['text'].split())
 
-    duplicates  = words1.intersection(words2)
-    uniques = words1.union(words2.difference(words1))
-    
+    duplicated = len(words1.intersection(words2))
+    uniques = len(words1.union(words2.difference(words1)))
+
     try:
-        simi = float(len(duplicates))/(len(uniques))
+        simi = float(duplicated) / uniques
         return simi
-    except:
-        print "Error to get similarity"
+    except ZeroDivisionError:
         return 0.0
+
 
 def neighborhood(point, tweets, eps):
     neighborPts = []
@@ -60,23 +59,24 @@ def neighborhood(point, tweets, eps):
         # distance = euclideanDistance(point, point2)
         # distance = fadingDistance(point, point2)
         distance = jaccardDistance(point, point2)
-        
+
         if distance > eps:
             neighborPts.append(point2);
     return neighborPts
 
+
 def expandCluster(tweets, point, neighborPts, cluster, eps, minPts):
     global progress
     point['cluster'] = cluster
+    point['core'] = True
 
-    for point1 in neighborPts:	
+    for point1 in neighborPts:
         if not point1['visited']:
             point1['visited'] = True
             neighborPts1 = neighborhood(point1, tweets, eps)
             progress = progress + 1
-            print progress
-            print " ----- Neibohood inside = " + str(len(neighborPts1))
             if len(neighborPts1) >= minPts:
+                point1['core'] = True
                 neighborPts.extend(neighborPts1)
         if point1['cluster'] == 0:
             point1['cluster'] = cluster
@@ -85,30 +85,26 @@ def expandCluster(tweets, point, neighborPts, cluster, eps, minPts):
 def dbScan(tweets, eps, minPts):
     global progress
     cluster = 0
-    
+
     for point in tweets:
         if not point['visited']:
             point['visited'] = True
             neighborPts = neighborhood(point, tweets, eps)
             progress = progress + 1
-            print progress
-            print " --- Neibohood = " + str(len(neighborPts))
             if len(neighborPts) < minPts:
                 point['cluster'] = -1
             else:
-                print " ----- Criou um cluster -----"
                 cluster += 1
-                # point['cluster'] = cluster
                 expandCluster(tweets, point, neighborPts, cluster, eps, minPts)
 
 
 print '- - - - - - - - - - START - - - - - - - - - -'
 
-#import json
+# import json
 import simplejson as json
 
 # Coloque o CAMINHO/NOME do seu arquivo de entrada dentro da funcao open()
-with open('tweets_30.tsv') as json_data:
+with open('tweets_22_05_pln.tsv') as json_data:
     # A linha abaixo le um arquivo em formato JSON
     # points = json.load(json_data)
     
@@ -117,22 +113,24 @@ with open('tweets_30.tsv') as json_data:
     points['tweets'] = []
     for line in json_data:
         data = line.split('\t')
+        if data[0] == 'id' and data[1] == 'text': continue
         point = {}
 
         # Aqui vc deve criar os atributos que voce ira utilizar no DBSCAN
         # Por exemplo: Na medida de fadding, voce ira precisar do atributo de tempo.
         point['id'] = data[0]
         point['text'] = data[1].strip()
-        point['time'] = data[2].strip()
-        points['tweets'].append(point)
-    
-
-    for point in points['tweets']:
+        # point['time'] = data[2].strip()
         point['visited'] = False
         point['cluster'] = 0
-    
+        point['core'] = False
+        points['tweets'].append(point)
+
+    # embaralhando os tweets
+    shuffle(points['tweets'])
+
     # A funcao DBSCAN recebe um array de pontos, eps e minPoints.
-    dbScan(points['tweets'], 0.3, 500)
+    dbScan(points['tweets'], 0.3, 15)
     
 
 
@@ -144,21 +142,14 @@ groups = defaultdict(list)
 for obj in points['tweets']:
     groups[obj['cluster']].append(obj)
 
-output_file = 0
-new_list = groups.values()
-quantCluster = len(new_list)
-
 # Caminho do arquivo de saida dos clusters
-output_file = open('/Users/LeonelJR/Documents/TCC/DBSCAN/result_30_eps03_min500.txt','a')
-for x in range(0, quantCluster):
-    print ' -> ' + str(new_list[x][0]['cluster']) + ': ' + str(len(new_list[x]))
-    for y in range(0, len(new_list[x])):
-        # if new_list[x][y]['cluster'] != -1:
-        #     print new_list[x][y]['id']
-        if new_list[x][y]['cluster'] != -1:
-            text = new_list[x][y]['text']
-            row = (text).encode('utf-8','ignore')+"\n"
-            output_file.write(row)
+output_file = open('result_u_22_eps03_min50.txt', 'w')
+for key in groups.keys():
+    print ' -> ' + str(groups[key][0]['cluster']) + ': ' + str(len(groups[key]))
+    for point in groups[key]:
+        text = point['text']
+        row = str(key) + "," + point['id'] + "," + text.encode('utf-8', 'ignore') + "," + str(point['core']) + "\n"
+        output_file.write(row)
 
 
 # print "######## CLUSTERS ########"
