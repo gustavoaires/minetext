b46c964bafc44d425795f6d37692f6039438063f
Gustavo Aires <gustavoairesmatos@gmail.com>
2017-09-05 22:59:42 -0300

    removing unnecessary file management

--- minetext/classifying/naivebayes.py
+++ minetext/classifying/naivebayes.py
@@ -1,93 +1,13 @@
-import nltk
-import os
-import csv
-import re
 import pandas
 import operator
 import math
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
 from sets import Set
-
-stemmer = SnowballStemmer("english")
-stopset = set(stopwords.words("english"))
-symbols = ["\"", "\'", "!", "?", ".", ",", ";", ">", "_", "<", "-", "[",
-           "]", "{", "}", "'s", "\/", "\\", "^", "~", "'", "`", "``",
-           ":", "(", ")", "@", "#", "$", "%", "&", "*", "=", "+"]
-
-pos_files = []
-neg_files = []
-
-incomplete_path = "/home/gustavo/git/sentiment_analysis/review_polarity/txt_sentoken/"
-
-# loading neg_files
-file_names = os.listdir(incomplete_path + "neg")
-neg_files = file_names[0:700]
-neg_files_test_set = file_names[700:1000]
-
-# loading pos_files
-file_names = os.listdir(incomplete_path + "pos")
-pos_files = file_names[0:700]
-pos_files_test_set = file_names[700:1000]
 
 training_set = []
 test_set = []
 
-# path to save training set
-training_set_path = '/home/gustavo/git/sentiment_analysis/training_set.csv'
-test_set_path = '/home/gustavo/git/sentiment_analysis/test_set.csv'
-test_set_path_classified = '/home/gustavo/git/sentiment_analysis/test_set_classified.csv'
-test_set_path_classified_ = '/home/gustavo/git/sentiment_analysis/test_set_classified_.csv'
-test_set_path_classified__ = '/home/gustavo/git/sentiment_analysis/test_set_classified__.csv'
-
-
-# reads the file
-def read_file(path, label):
-    with open(path, 'r') as text_file:
-        text = text_file.read()
-        tokens = str(text).split()
-        valid_tokens = [w for w in tokens if (w not in stopset) and (w not in symbols) and (re.search('[a-zA-Z]', w))]
-        tokens = []
-        for token in valid_tokens:
-            tokens.append(str(stemmer.stem(token)))
-        training_set.append((tokens, label))
-
-
-# iterate all file names to read and create tuples
-def get_file_name():
-    for i in range(0, 700):
-        path = incomplete_path + "neg/" + neg_files[i]
-        read_file(path, 'neg')
-        path = incomplete_path + "pos/" + pos_files[i]
-        read_file(path, 'pos')
-
-
-# save data (tokens with labels) into file
-def write_training_set():
-    with open(training_set_path, 'wb') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in training_set:
-            data = []
-            for w in row[0]:
-                data.append(w)
-            data.append(row[1])
-            writer.writerow(data)
-
-
 # words to indexing data frame
 words = Set()
-
-
-# read training set file
-def read_training_set():
-    with open(training_set_path, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile)
-        for row in spamreader:
-            training_set.append((row[0:-1], row[-1]))
-            for w in row[0:-1]:
-                words.add(w)
-
 
 # data frame with the frequency
 df = pandas.DataFrame(columns=['neg', 'pos'], index=words)
@@ -109,26 +29,6 @@ def count_freq():
     return df
 
 
-# path to data frame csv
-data_frame_path = '/home/gustavo/git/sentiment_analysis/data_frame__.csv'
-removed_words_path = '/home/gustavo/git/sentiment_analysis/removed_words__.csv'
-
-
-# write data frame into csv file
-def write_data_frame(df):
-    data = df[-12000:]
-    data2 = df[0:-12000]
-    data.to_csv(data_frame_path, sep='\t')
-    data2.to_csv(removed_words_path, sep='\t')
-    return data
-
-
-# read data frame from csv file
-def read_data_frame():
-    df = pandas.read_csv(data_frame_path, sep='\t')
-    return df
-
-
 # class probabilities
 class_prob_pos = 0.0
 class_prob_neg = 0.0
@@ -146,50 +46,9 @@ def calculate_class_probability(df):
         total = total + 1.0
     class_prob_neg = count_neg / total
     class_prob_pos = count_pos / total
-    # aux = 1 - (class_prob_neg + class_prob_pos)
-    # + aux / 2
     class_prob_neg = class_prob_neg
     class_prob_pos = class_prob_pos
     return class_prob_neg, class_prob_pos
-
-
-# create list with data from test set file
-def read_file_test_set(path, label):
-    with open(path, 'r') as text_file:
-        text = text_file.read()
-        tokens = str(text).split()
-        valid_tokens = [w for w in tokens if (w not in stopset) and (w not in symbols) and (re.search('[a-zA-Z]', w))]
-        tokens = []
-        for token in valid_tokens:
-            tokens.append(str(stemmer.stem(token)))
-        # adding path for further identification
-        tokens.append(path)
-        test_set.append(tokens)
-
-
-# write test_set
-def write_test_set():
-    with open(test_set_path, 'wb') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in test_set:
-            writer.writerow(row)
-
-
-# write test_set classified
-def write_test_set_classified():
-    with open(test_set_path_classified__, 'wb') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in test_set:
-            writer.writerow(row)
-
-
-# create test set
-def create_test_set():
-    for i in range(0, 300):
-        path = incomplete_path + "neg/" + neg_files_test_set[i]
-        read_file_test_set(path, 'neg')
-        path = incomplete_path + "pos/" + pos_files_test_set[i]
-        read_file_test_set(path, 'pos')
 
 
 # classifier
@@ -216,10 +75,7 @@ def naive_bayes_classify(document, labels, processed_words, class_probabilities,
 
 
 def main():
-    read_training_set()
     df = count_freq()
-    # write_data_frame(df)
-    create_test_set()
 
     dataframe = df[-12000:]
 
@@ -237,30 +93,9 @@ def main():
     for label in labels:
         words_per_class[label] = df[label].sum()
 
-    qtd_pos = 0
-    qtd_neg = 0
-    neg = incomplete_path + "neg/"
-    pos = incomplete_path + "pos/"
-    errou_neg = 0
-    errou_pos = 0
     for document in test_set:
         classification = naive_bayes_classify(document[:-1], labels, processed_words, class_probabilities,
                                               words_per_class, dataframe)
-        if classification == 'neg':
-            qtd_neg += 1
-            if neg not in document[-1]:
-                errou_neg += 1
-        else:
-            qtd_pos += 1
-            if pos not in document[-1]:
-                errou_pos += 1
-        print classification
         document.append(classification)
-    write_test_set_classified()
-    print dataframe
-    print qtd_pos, ' - ', qtd_neg
-    print errou_pos, ' - ', errou_neg
-    print class_prob_neg, ' - ', class_prob_pos
-
 
 main()
