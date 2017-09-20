f35b12ed76cb40cac5f638973a4bad57e248b12b
Gustavo Aires <gustavoairesmatos@gmail.com>
2017-09-19 23:51:50 -0300

    small fixies, renaming variable, code refactoring

--- minetext/classifying/naivebayes.py
+++ minetext/classifying/naivebayes.py
@@ -12,16 +12,16 @@ class NaiveBayes(object):
         self.text_field_name = text_field_name
         self.labels = labels
         # words for indexing data frame
-        self.words = Set()
-        self.load_words_set()
+        self.words_set = Set()
         # data frame with the frequency
-        self.df = pandas.DataFrame(columns=labels, index=self.words)
+        self.df = pandas.DataFrame(columns=labels, index=self.words_set)
         self.classes_prob = self.assign_zero_to_classes()
+        self.words_per_class = dict()
 
     def load_words_set(self):
         for document in self.training_set:
             for word in document.split():
-                self.words.add(word)
+                self.words_set.add(word)
 
     def count_freq(self):
         words = Set()
@@ -44,6 +44,10 @@ class NaiveBayes(object):
             classes_prob[label] = 0.0
         return classes_prob
 
+    def calculate_words_per_class(self):
+        for label in self.labels:
+            self.words_per_class[label] = self.df[label].sum()
+
     def calculate_class_probability(self):
         class_counts = self.assign_zero_to_classes()
         total = 0.0
@@ -54,14 +58,14 @@ class NaiveBayes(object):
             self.classes_prob[key] = class_counts[key] / total
         return self.classes_prob
 
-    def naive_bayes(self, document, processed_words, words_per_class):
+    def naive_bayes(self, document):
         no_words_in_doc = len(document[self.text_field_name].split())
-        current_class_prob = dict()
+        prob_per_class = dict()
 
         for label in self.labels:
-            prob = math.log(self.classes_prob[label], 2) - no_words_in_doc * math.log(words_per_class[label], 2)
+            prob = math.log(self.classes_prob[label], 2) - no_words_in_doc * math.log(self.words_per_class[label], 2)
             for word in document:
-                if word in processed_words:
+                if word in self.words_set:
                     occurence = self.df.loc[word][label]
                     if occurence > 0:
                         prob = prob + math.log(occurence, 2)
@@ -69,29 +73,23 @@ class NaiveBayes(object):
                         prob = prob + math.log(1, 2)
                 else:
                     prob = prob + math.log(1, 2)
-            current_class_prob[label] = prob
+            prob_per_class[label] = prob
 
-        sorted_labels = sorted(current_class_prob.items(), key=operator.itemgetter(1))
-        most_probable_class = sorted_labels[-1][0]
+        sorted_classes = sorted(prob_per_class.items(), key=operator.itemgetter(1))
+        most_probable_class = sorted_classes[-1][0]
         return most_probable_class
 
-    def classify(self):
+    def run(self):
+        self.load_words_set()
         self.count_freq()
-
-        processed_words = []
-
         self.calculate_class_probability()
-
-        for word in self.df.index:
-            processed_words.append(word)
-
-        words_per_class = dict()
-
-        for label in self.labels:
-            words_per_class[label] = self.df[label].sum()
+        self.calculate_words_per_class()
 
         for document in self.test_set:
-            classification = self.naive_bayes_classify(document, processed_words, words_per_class)
-            document['predicted_class'] = classification
+            self.classify(document)
 
         return self.test_set
+
+    def classify(self, document):
+        document['predicted_class'] = self.naive_bayes(document)
+        return document
