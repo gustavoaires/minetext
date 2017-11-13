1721e704b6622f86b398acd0aa6a6b5f94be836c
Gustavo Aires <gustavoaires@trixlog.com>
2017-11-12 23:23:32 -0300

    convert comparable attribute to string before comparing

--- minetext/visualization/utils.py
+++ minetext/visualization/utils.py
@@ -1,8 +1,8 @@
-def match_documents(source, target, comparable_field_source="id", comparable_field_target="id_str"):
+def match_documents(source, target, comparable_field="id"):
     matched_documents = list()
     for document in source[:1]:
         for target_document in target[:50]:
-            if document[comparable_field_source] == target_document[comparable_field_target]:
+            if str(document[comparable_field]) == str(target_document[comparable_field]):
                 matched_documents.append(target_document)
                 break
     return matched_documents
